1da86fbfeb73dc00e47045e7661cd95312b09835
Luis Locon <luis.locon@gmail.com>
2020-05-17 00:20:45 -0600

    Randomize a Dice Images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,24 +2,29 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Luis Locon on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Luis Locon on 17/05/2020.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    // IBOutlet is a variable for of the UI can Interact
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        diceImageViewOne.alpha = 0.5;
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo");
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    // This describes an action, when button was touched by User
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // This a form to randomize a number;
+        // let leftDiceNumber = Int.random(in: 0...5)
+        // let rightDiceNumber = Int.random(in: 0...5)
+        let imagesArr = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ];
+        // How to define an array
+        // This form retuns a random element from the array from 0 to length
+        diceImageViewOne.image = imagesArr.randomElement()
+        diceImageViewTwo.image = imagesArr[rightDiceNumber]
     }
-
-
 }
 
