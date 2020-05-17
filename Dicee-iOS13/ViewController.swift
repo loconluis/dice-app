9ce732dd442fef1b38b9f884c1a16380267bcba8
Luis Locon <luis.locon@gmail.com>
2020-05-16 19:21:28 -0600

    Link our Design to our code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
+//  Created by Luis Locon on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.alpha = 0.5;
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo");
     }
 
 
